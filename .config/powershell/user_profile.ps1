ab2c60f2413364add11237d3cdb0677ba76a8fa5
igmtink <117252369+igmtink@users.noreply.github.com>
2022-11-11 03:09:56 +0800

    Update user_profile.ps1

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -1,4 +1,26 @@
+# set PowerShell to UTF-8
+[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+
+# Oh My Posh
 oh-my-posh init pwsh | Invoke-Expression
 oh-my-posh init pwsh --config "C:\Users\igmts\.config\powershell\igmtink.omp.json" | Invoke-Expression
 
 Import-Module -Name Terminal-Icons
+
+# PSReadLine
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
