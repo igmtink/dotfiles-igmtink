f65705b8055a569cb005561a64676aefbde01dc7
igmtink <117252369+igmtink@users.noreply.github.com>
2022-11-11 02:06:47 +0800

    Create user_profile.ps1

---
+++ .config/powershell/user_profile.ps1
@@ -0,0 +1,4 @@
+oh-my-posh init pwsh | Invoke-Expression
+oh-my-posh init pwsh --config "C:\Users\igmts\.config\powershell\igmtink.omp.json" | Invoke-Expression
+
+Import-Module -Name Terminal-Icons
