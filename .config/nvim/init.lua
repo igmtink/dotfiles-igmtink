1f012e53ca841a0fc3fcefa7b200080d21c80911
igmtink <117252369+igmtink@users.noreply.github.com>
2022-11-12 18:24:40 +0800

    Delete init.lua

--- .config/nvim/init.lua
+++
@@ -1,7 +0,0 @@
-require "igmtink.plugins-setup" -- Shoud be at the top for loading the package manager first
-require "igmtink.core.options"
-require "igmtink.core.keymaps"
-require "igmtink.core.colorscheme"
-require "igmtink.plugins.comment"
-require "igmtink.plugins.nvim-tree"
-require "igmtink.plugins.lualine"
