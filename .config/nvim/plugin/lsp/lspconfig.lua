e19a2091668cdeeb9ad7ee8f07b1d948f4994f7d
igmtink <117252369+igmtink@users.noreply.github.com>
2022-11-18 15:29:06 +0800

    Update lspconfig.lua

--- .config/nvim/plugin/lsp/lspconfig.lua
+++ .config/nvim/plugin/lsp/lspconfig.lua
@@ -124,7 +124,7 @@ nvim_lsp.cssls.setup {
 nvim_lsp.emmet_ls.setup({
   capabilities = capabilities,
   on_attach = on_attach,
-  filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte", "ejs" },
+  filetypes = { "html", "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx", "ejs", "css", "sass", "scss", "less", "svelte", "ejs" },
 })
 
 nvim_lsp.astro.setup {
