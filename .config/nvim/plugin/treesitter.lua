417dae306f463f32c45d3bb6af7416b2d8189ea2
igmtink <117252369+igmtink@users.noreply.github.com>
2022-11-18 12:44:25 +0800

    Update treesitter.lua

--- .config/nvim/plugin/treesitter.lua
+++ .config/nvim/plugin/treesitter.lua
@@ -1,33 +1,35 @@
 local status, ts = pcall(require, "nvim-treesitter.configs")
-if not status then
-	return
-end
+if (not status) then return end
 
-ts.setup({
-	highlight = {
-		enable = true,
-		disable = {},
-	},
-	indent = {
-		enable = true,
-		disable = {},
-	},
-	ensure_installed = {
-		"tsx",
-		"javascript",
-		"typescript",
-		"json",
-		"css",
-		"html",
-		"lua",
-		"astro",
-		"gitignore",
-	},
-	autotag = {
-		enable = true,
-	},
-	auto_install = true,
-})
+ts.setup {
+  highlight = {
+    enable = true,
+    disable = {},
+  },
+  indent = {
+    enable = true,
+    disable = {},
+  },
+  ensure_installed = {
+    "tsx",
+    "javascript",
+    "typescript",
+    "toml",
+    "fish",
+    "php",
+    "json",
+    "yaml",
+    "swift",
+    "css",
+    "html",
+    "astro",
+    "gitignore",
+    "lua"
+  },
+  autotag = {
+    enable = true,
+  },
+}
 
-local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
 parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
