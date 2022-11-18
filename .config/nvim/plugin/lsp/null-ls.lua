e17cc34d6fbef978f2fab9613cf8f643268efd37
igmtink <117252369+igmtink@users.noreply.github.com>
2022-11-18 12:47:25 +0800

    Update null-ls.lua

--- .config/nvim/plugin/lsp/null-ls.lua
+++ .config/nvim/plugin/lsp/null-ls.lua
@@ -1,51 +1,47 @@
--- import null-ls plugin safely
-local setup, null_ls = pcall(require, "null-ls")
-if not setup then
-  return
-end
+local status, null_ls = pcall(require, "null-ls")
+if (not status) then return end
 
--- for conciseness
-local formatting = null_ls.builtins.formatting -- to setup formatters
-local diagnostics = null_ls.builtins.diagnostics -- to setup linters
-
--- to setup format on save
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
--- configure null_ls
-null_ls.setup({
-  -- setup formatters & linters
+local lsp_formatting = function(bufnr)
+  vim.lsp.buf.format({
+    filter = function(client)
+      return client.name == "null-ls"
+    end,
+    bufnr = bufnr,
+  })
+end
+
+null_ls.setup {
   sources = {
-    --  to disable file types use
-    --  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
-    formatting.prettierd, -- js/ts formatter
-    formatting.stylua, -- lua formatter
-    diagnostics.eslint_d.with({ -- js/ts linter
-
-      -- diagnostics_format = "[eslint] #{m}\n(#{c})",
-
+    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.diagnostics.eslint_d.with({
+      -- diagnostics_format = '[eslint] #{m}\n(#{c})'
       -- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
       condition = function(utils)
         return utils.root_has_file(".eslintrc.js") -- change file extension if you use something else
       end,
     }),
+    null_ls.builtins.diagnostics.fish
   },
-  -- configure format on save
-  on_attach = function(current_client, bufnr)
-    if current_client.supports_method("textDocument/formatting") then
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
       vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
       vim.api.nvim_create_autocmd("BufWritePre", {
         group = augroup,
         buffer = bufnr,
         callback = function()
-          vim.lsp.buf.format({
-            filter = function(client)
-              --  only use null-ls for formatting instead of lsp server
-              return client.name == "null-ls"
-            end,
-            bufnr = bufnr,
-          })
+          lsp_formatting(bufnr)
         end,
       })
     end
+  end
+}
+
+vim.api.nvim_create_user_command(
+  'DisableLspFormatting',
+  function()
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = 0 })
   end,
-})
+  { nargs = 0 }
+)
